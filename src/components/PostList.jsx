378316f902e40eeccf992c31a5b0bb6b2dde41c3
Benjamin Inglis <bjammin134@gmail.com>
2025-06-25 09:30:48 -0700

    Load posts from backend and add loading state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -7,6 +7,23 @@ import classes from "./PostList.module.css";
 
 export default function PostList({ isPosting, handleHideModal }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const res = await fetch("http://localhost:8080/posts");
+
+      if (!res.ok) {
+        throw new Error("An error occurred when fetching the data");
+      }
+
+      const resData = await res.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
   function handleAddPost(postData) {
     fetch("http://localhost:8080/posts", {
@@ -26,7 +43,7 @@ export default function PostList({ isPosting, handleHideModal }) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post
@@ -38,10 +55,15 @@ export default function PostList({ isPosting, handleHideModal }) {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet. </h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
