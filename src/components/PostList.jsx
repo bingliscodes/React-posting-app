bc13866fee88894725403e49c365bcb964c5f4ba
Benjamin Inglis <bjammin134@gmail.com>
2025-06-25 09:06:50 -0700

    Add message for when no posts available

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -19,15 +19,23 @@ export default function PostList({ isPosting, handleHideModal }) {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post
-            key={post.content}
-            content={post.content}
-            author={post.author}
-          />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post
+              key={post.content}
+              content={post.content}
+              author={post.author}
+            />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet. </h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
